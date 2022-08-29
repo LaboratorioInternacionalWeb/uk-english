d0026af084ff866938ef059eef5f9ab59d2738bc
Víctor Osornio <victor@labweb.mx>
2022-08-29 11:32:33 -0500

    First version of this fresh experiment

---
+++ islands/Body.tsx
@@ -0,0 +1,81 @@
+/** @jsx h */
+import { h } from "preact";
+import { useState,useEffect } from "preact/hooks";
+import { tw } from "@twind";
+import Services from "./Services.tsx";
+import Levels from "./Levels.tsx";
+
+import { Button } from "../components/Button.tsx";
+
+
+export default function Body() {
+  const [topic, setTopic] = useState("");
+  const [people, setPeople] = useState("Toddlers");
+
+
+  
+  useEffect(() => {
+    if(people==="Juniors" || people==="Adults" || topic==="English")
+      document.getElementById("Scroll")?.scrollIntoView({behavior: "smooth", block: "end", inline: "end"});
+  }, [people, topic]);
+
+  function handleToeflButtonClick() {
+    setTopic("Toefl")
+    setPeople("Toddlers")
+  }
+ 
+  return (
+          <div>
+            {/* Main content */}
+            <div class={tw`flex flex-col md:flex-row md:pr-20`}>
+              {/* Image */}
+              <div class={tw`md:mx-20 md:mt-5 h-full w-full rounded-b-3xl md:py-10`}>
+                <img class={tw`object-cover `}
+                    src="/british.png"
+                    height="100px"
+                    alt="Uk english logo: a Red Uk black english letters"
+                />
+            </div>
+              {/* Title */}
+              <div class={tw`md:mt-5 md:pl-30 md:pt-10 flex flex-col justify-center`}>
+              <h1 class={tw`font-mono subpixel-antialiased text-4xl md:text-5xl font-bold uppercase text-center`}>Study English Online</h1>
+              <p class={tw`font-mono subpixel-antialiased text-xl md:text-xl md:mt-10 font-bold text-center`}>
+                We offer you step-by-step courses of English for making you learning process easier and your skills more proficient. 
+                Chose the program that suits you the best by checking the program that suits you best by checking your level of English.
+              </p>
+              {/* Buttons */}
+              <div class={tw`flex flex-col md:flex-row md:mt-10 justify-around`}>
+                <button class={tw`bg-[#c83a3d] hover:bg-[#de5357] md:text-2xl text-white font-bold md:py-8 md:px-20 rounded-full`} onClick={handleToeflButtonClick}>Toefl</button>
+                <button class={tw`bg-[#c83a3d] hover:bg-[#de5357] md:text-2xl text-white font-bold md:py-8 md:px-16 rounded-full`} onClick={() => setTopic("English")} href="English">English</button>
+              </div>
+            </div>
+            </div>
+            {/* Services */}
+            <div id="Scroll" >
+              <div class={tw`${topic==="English"?"":"hidden"} w-full md:w-3/4 md:ml-40 md:mb-8 bg-white rounded-lg border shadow-md dark:bg-gray-800 dark:border-gray-700`} >
+                <ul class={tw`flex flex-wrap text-sm font-medium text-center text-gray-500 bg-gray-50 rounded-t-lg border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800`} id="defaultTab" data-tabs-toggle="#defaultTabContent" role="tablist">
+                  <li class={tw`mr-2`}>
+                  <button onClick={() => setPeople("Toddlers")} id="about-tab" data-tabs-target="#about" type="button" role="tab" aria-controls="about" aria-selected="true" class={tw`inline-block p-4 text-blue-600 rounded-tl-lg hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-blue-500`}>
+                    Toddlers
+                  </button>
+                </li>
+                  <li class={tw`mr-2`}>
+                  <button onClick={() => setPeople("Juniors")} id="services-tab" data-tabs-target="#services" type="button" role="tab" aria-controls="services" aria-selected="false" class={tw`inline-block p-4 hover:text-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-gray-300`}>
+                    Juniors
+                  </button>
+                </li>
+                  <li class={tw`mr-2`}>
+                    <button onClick={() => setPeople("Adults")} id="statistics-tab" data-tabs-target="#statistics" type="button" role="tab" aria-controls="statistics" aria-selected="false" class={tw`inline-block p-4 hover:text-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-gray-300`}>
+                    Adults
+                  </button>
+                  </li>
+                </ul>
+                <Services people={people}/>
+              </div>
+              <Levels people={people}/>
+              {/* Phantom */}
+              <div class={tw`m-10`} ></div>
+            </div>
+          </div>
+  );
+}
---
+++ routes/index.tsx
@@ -0,0 +1,41 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw, setup } from "@twind";
+import { useState } from "preact/hooks";
+import Body from "../islands/Body.tsx";
+
+export default function Home() {
+  return (
+    //Full page
+    <div class={tw`h-screen bg-[#777c7c03]`}>
+      {/* Phantom */}
+      <div class={tw`md:pt-10`}></div>
+      {/* Heather */}
+      <div class={tw`md:py-6  md:pl-14 mx-4 bg-gradient-to-t from-[#ffffff] via-[#ffffff] from-[#dc1d1d14] shadow-lg border-y-2 border-gray-100 py-6 rounded-3xl `}>
+        {/* Menu */}
+        <div class={tw`flex flex-row justify-between md:justify-between m-1`}>
+          {/* Img */}
+          <div class={tw`mt-3`}>
+              <img class={tw`object-cover h-10 w-30`}
+                src="/logo.svg"
+                height="100px"
+                alt="Uk english logo: a Red Uk black english letters"
+              />
+          </div>
+          <div class={tw`bg-[#4a9ab0] hover:bg-[#4a9ab07a] md:text-xl text-white font-bold md:py-4 md:px-8 mr-4 rounded-full`}>
+            <p>Start Now</p>
+          </div>
+        </div>
+      </div>
+      {/* Body */}
+      <div class={tw`mx-2`}>
+        {/* Main content */}
+        <Body/>
+      {/* Footer */}
+      <div class={tw``}>
+        <p>uk-english</p>
+      </div>
+    </div>
+    </div>
+  );
+}
